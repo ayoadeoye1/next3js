d74b555317c6bb3dbde62936a0cdf18adb88f079
ayoadeoye1 <ayoadeoye.isaac@gmail.com>
2025-06-28 20:26:44 +0100

    feat: fixed eslint issue and cd errors ++

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
+
+  output: "export",
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+
+  experimental: {
+    esmExternals: true,
+  },
 };
 
 export default nextConfig;
