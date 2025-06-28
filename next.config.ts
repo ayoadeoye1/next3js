ea51f523bf5626aae853c38dfdd90ad2364d625d
ayoadeoye1 <ayoadeoye.isaac@gmail.com>
2025-06-28 20:49:34 +0100

    feat: deployent fix

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
 import type { NextConfig } from "next";
 
-// Check if we're in production and deploying to GitHub Pages
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const basePath = isGithubPages ? "/next3portfolio" : "";
-
 const nextConfig: NextConfig = {
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
 
   output: "export",
   trailingSlash: true,
 
-  // Set base path for GitHub Pages
-  basePath: basePath,
-  assetPrefix: basePath,
+  basePath: "/next3js",
+  assetPrefix: "/next3js/",
 
   images: {
     unoptimized: true,
